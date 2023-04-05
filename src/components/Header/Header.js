5348e44f82b192831bafc9d764619eebd70300f1
Ruben Sanromán <ruben.sanroman@gmail.com>
2023-04-05 14:15:38 -0400

    Basic architecture and layout structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header id={styles.header}>
+      <h1>Financial Report</h1>
+    </header>
+  )
+}
+
+export default Header;
