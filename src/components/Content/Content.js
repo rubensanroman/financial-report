280775e023d06dd98278ce49d8013af4bdc9306a
Ruben Sanromán <ruben.sanroman@gmail.com>
2023-04-05 15:29:24 -0400

    New logo and a cooler app name

---
+++ src/components/Content/Content.js
@@ -0,0 +1,13 @@
+import styles from "./Content.module.scss";
+
+const Content = () => {
+  return (
+    <main id={styles.content}>
+      <aside>
+        <h1>Content</h1>
+      </aside>
+    </main>
+  );
+};
+
+export default Content;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,20 @@
-import Image from 'next/image'
-import styles from './Header.module.scss';
+import Image from "next/image";
+import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
     <header id={styles.header}>
       <div id={styles.logo}>
-        <Image
-          alt="Financial Report"
-          height={100}
-          src="/icon-finances.png"
-          width={100}
-        />
+        <div className={styles.logoWrapper}>
+          <Image alt="Wealth Chimp" height={80} src="/logo.png" width={80} />
+        </div>
         <div id="styles.title">
-          <h1>Financial Report</h1>
+          <h1>Wealth Chimp</h1>
           <p>Streamline your finances effortlessly.</p>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
