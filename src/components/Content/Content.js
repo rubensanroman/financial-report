425f589cef63314a7e94c4fe9f1ef124673c9fd0
Ruben Sanromán <ruben.sanroman@gmail.com>
2023-04-05 16:32:39 -0400

    Dynamic logo height with SASS variables

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -3,9 +3,24 @@ import styles from "./Content.module.scss";
 const Content = () => {
   return (
     <main id={styles.content}>
-      <aside>
+      <aside id={styles.sidebar}>
         <h1>Content</h1>
       </aside>
+      <main id={styles.main}>
+        <div className={styles.inner}>
+          <table>
+            <thead>
+              <tr>
+                <th>Oct 2022</th>
+                <th>Nov 2022</th>
+                <th>Dec 2022</th>
+                <th>Jan 2023</th>
+                <th>Feb 2023</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </main>
     </main>
   );
 };
