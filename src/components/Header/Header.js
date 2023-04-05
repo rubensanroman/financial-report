aa8a27dbb26ae78e87713512e2c7cfda6a4db78e
Ruben Sanromán <ruben.sanroman@gmail.com>
2023-04-05 14:31:56 -0400

    Added a logo image and a slogan

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
+import Image from 'next/image'
 import styles from './Header.module.scss';
 
 const Header = () => {
   return (
     <header id={styles.header}>
-      <h1>Financial Report</h1>
+      <div id={styles.logo}>
+        <Image
+          alt="Financial Report"
+          height={100}
+          src="/icon-finances.png"
+          width={100}
+        />
+        <div id="styles.title">
+          <h1>Financial Report</h1>
+          <p>Streamline your finances effortlessly.</p>
+        </div>
+      </div>
     </header>
   )
 }
